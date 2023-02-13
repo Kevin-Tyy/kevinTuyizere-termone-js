7c361369e93cb8fc864f7cfaa013cc4e69075954
Kevin Tuyizere <tuyizerek27@gmail.com>
2023-02-13 15:41:57 +0200

    init

--- app.js
+++ app.js
@@ -24,16 +24,8 @@ app.post('/sum', (req,res)=>{
     const number5 =parseInt(req.body.number5);
 
     const arr = [number1, number2, number3, number4, number5];
-    
-    console.log(number1);
-    console.log(number2);
-    console.log(number3);
-    console.log(number4);
-    console.log(number5);
-
 
     let  sum = 0;
-
     for(let i = 0; i < arr.length; i++){
         sum += arr[i];  
     }
